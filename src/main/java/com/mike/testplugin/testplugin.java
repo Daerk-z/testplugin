84b1b1d1724321b9b8ca75c622ffe07172caf0c7
MichaelXII12 <mm6537660@gmail.com>
2025-08-13 16:14:58 -0500

    Test

--- src/main/java/com/mike/testplugin/testplugin.java
+++ src/main/java/com/mike/testplugin/testplugin.java
@@ -1,7 +1,17 @@
 package com.mike.testplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mike.testplugin.tools.MessageColors;
+
 public class testplugin extends JavaPlugin {
+
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(MessageColors.coloredMessage("&aTestPlugin has been enabled."));
+    }
     
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(MessageColors.coloredMessage("&cTestPlugin has been disabled."));
+    }
 }
